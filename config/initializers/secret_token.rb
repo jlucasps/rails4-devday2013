85be5a39a55f17a3f1c348be4670c28cc4472db0
João Lucas <jlucasps@gmail.com>
2013-10-18 15:20:30 -0300

    Adicionando bootstrap 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4chat::Application.config.secret_key_base = 'fc4165c2cf391c22be4287e5b8b8be92a80c3e7435cb896e5b694ed7c6361ad8ab00184a9509e8aea770977a2a6cea40a43b2bf455bc15375c3517b26cdbb95e'
